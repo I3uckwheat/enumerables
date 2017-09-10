74eb02a8a5504405f140a7fbe67f25440a021ea8
Briggs Elsperger <briggs.elsperger@hotmail.com>
2017-09-10 14:35:38 -0500

    finish my_each

--- enum.rb
+++ enum.rb
@@ -1,5 +1,20 @@
 module Enumerable
   def my_each
-    #codeHERE
+    index = 0
+    while index < self.length
+      yield(self[index])
+      index += 1
+    end
+   self
   end
-end
+
+  def my_each_with_index
+    index = 0
+    while index < self.length
+      yield(self[index], index)
+      index += 1
+    end
+    self
+  end
+
+end #end of module
