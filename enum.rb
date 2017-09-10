ef209e43fcd2af7f890f7b9bfa5d35d684c0d63d
Briggs Elsperger <briggs.elsperger@hotmail.com>
2017-09-10 14:46:23 -0500

    add my_each_with_index

--- enum.rb
+++ enum.rb
@@ -1,20 +1,19 @@
 module Enumerable
   def my_each
     index = 0
-    while index < self.length
+    while index < length
       yield(self[index])
-      index += 1
-    end
-   self
-  end
-
-  def my_each_with_index
-    index = 0
-    while index < self.length
-      yield(self[index], index)
       index += 1
     end
     self
   end
 
-end #end of module
+  def my_each_with_index
+    index = 0
+    each do
+      yield(self[index], index)
+      index += 1
+    end
+    self
+  end
+end # end of module
